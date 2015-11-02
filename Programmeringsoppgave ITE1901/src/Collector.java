f4fd39fc5dc70a61bea2dcfd528df1c7a0e522e3
Christian Petersen <501230@student.hin.no>
2015-11-02 18:21:15 +0100

    Initial commit

---
+++ Programmeringsoppgave ITE1901/src/Collector.java
@@ -0,0 +1,8 @@
+
+public class Collector {
+
+	public Collector() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Programmeringsoppgave ITE1901/src/Converter.java
@@ -0,0 +1,8 @@
+
+public class Converter {
+
+	public Converter() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
